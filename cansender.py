c43b7e5912a2c2e5c6d9d69273d7c9e54cd40bb7
andreas <andreas.luethi@gmx.net>
2020-01-15 22:45:18 +0100

    Try to to stop the reader!

--- cansender.py
+++ cansender.py
@@ -6,7 +6,6 @@ from can import Bus, MessageSync
 from player2 import LogReader2
 
 
-
 class CanSender(threading.Thread):
     def __init__(self, infile, channel, interface, *args, start_time=0.0, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,12 +14,11 @@ class CanSender(threading.Thread):
         self.config = {'single_handle': True}
         self.config['interface'] = interface
         self.bus = Bus(channel, **self.config)
-
         self.runevent = threading.Event()
         self.runevent.clear()
-
         self.killevent = threading.Event()
         self.killevent.set()
+        self.reader = None
 
     def run(self):
         while True:
@@ -36,12 +34,19 @@ class CanSender(threading.Thread):
             finally:
                 pass
 
-            self.reader.stop()
-            sleep(0.1)
-            self.reader.__exit__()
+            if self.reader:
+                self.stop_reader()
             self.runevent.clear()
             if not self.killevent.isSet():
                 break
+
+    def stop_reader(self):
+        try:
+            self.reader.stop()
+            sleep(0.2)
+            self.reader.__exit__()
+        finally:
+            self.reader = None
 
     def resume(self, start_time):
         self.start_time = start_time
@@ -52,7 +57,8 @@ class CanSender(threading.Thread):
 
     def exit(self):
         self.runevent.clear()
-        self.reader.stop()
+        if self.reader:
+            self.stop_reader()
         self.killevent.clear()
         threading.Thread.join(self)
 

888edfb971c67fd1fe0457e3a5669a5836ff96b5
andreas <andreas.luethi@gmx.net>
2020-01-30 21:08:34 +0100

    Update help text.

--- helptext.py
+++ helptext.py
@@ -1,30 +1,30 @@
 HELP_CONFIG_FILE = '''Configuration file in json format.
-                        for example:
-                        {
-                           "canbus": {
-                              "channel": "vcan0",
-                              "interface": "socketcan"
-                           },
-                           "canlog": {
-                              "date": "2019-10-21",
-                              "filename": "data/candump-2019-09-21_110938-gps.db"
-                           },
-                           "description": "Flug vom 2019-10-21",
-                           "video": {
-                              "aspectratio": 2,
-                              "bookmarks": [
-                                 14880,
-                                 35203,
-                                 45789,
-                                 217600,
-                                 235355
-                              ],
-                              "filename": "data/total-Aug-audio-x264.mp4",
-                              "fps": 30,
-                              "syncpoints": {
-                                 "14881": 1569062280.0,
-                                 "217601": 1569069060.0
-                              }
-                           }
-                        }
-                        '''
+{
+   "canbus": {
+      "channel": "vcan0",
+      "interface": "socketcan"
+   },
+   "canlog": {
+      "date": "2019-10-21",
+      "filename": "data/candump-2019-09-21_110938-gps.db"
+   },
+   "description": "Flug vom 2019-10-21",
+   "video": {
+      "bookmarks": [
+         120,
+         408,
+         432,
+         597,
+         600,
+         1800,
+         2373,
+         3600,
+         7200
+      ],
+      "filename": "data/total-Aug-audio-x264.mp4",
+      "syncpoints": {
+         "496": 1569062318.0,
+         "7253": 1569069060.0
+      }
+   }
+}'''
